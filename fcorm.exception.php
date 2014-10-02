62ea453fb660849b1ae9c6191d84860f742dee3f
Flavien Collomb <social@flaviencollomb.fr>
2014-10-02 14:25:16 +0200

    Create fcorm.exception.php

---
+++ fcorm.exception.php
@@ -0,0 +1,9 @@
+class InvalidConnectorException extends Exception {}
+class InvalidIdException extends Exception {}
+class InvalidFcORMException extends Exception {}
+class InvalidFieldException extends Exception {}
+class NotUniqueIdException extends Exception {}
+class ErrorLoadException extends Exception {}
+class ErrorInsertException extends Exception {}
+class ErrorUpdateException extends Exception {}
+class ErrorDeleteException extends Exception {}
